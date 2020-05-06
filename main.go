faa47208bde1a7fe6babe1ed36d52354fbba6c9c
Kazue Sasatani <sasatanikazue@Kazues-MacBook-Air.local>
2020-05-06 16:45:48 +0900

    Updated session

--- main.go
+++ main.go
@@ -4,14 +4,55 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/sessions"
+	"github.com/pkg/errors"
 	"github.com/tenntenn/sqlite"
 )
 
+var (
+	sessionStore sessions.Store
+)
+
+func determineEncryptionKey() ([]byte, error) {
+	sek := os.Getenv("SESSION_ENCRYPTION_KEY")
+	lek := len(sek)
+	switch {
+	case lek >= 0 && lek < 16, lek > 16 && lek < 24, lek > 24 && lek < 32:
+		return nil, errors.Errorf("SESSION_ENCRYPTION_KEY needs to be either 16, 24 or 32 characters long or longer, was: %d", lek)
+	case lek == 16, lek == 24, lek == 32:
+		return []byte(sek), nil
+	case lek > 32:
+		return []byte(sek[0:32]), nil
+	default:
+		return nil, errors.New("invalid SESSION_ENCRYPTION_KEY: " + sek)
+	}
+
+}
+
+func handleSessionError(w http.ResponseWriter, err error) {
+	// log.Info("Error handling session.")
+	http.Error(w, "Application Error", http.StatusInternalServerError)
+}
+
 func main() {
+
+	ek, err := determineEncryptionKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	sessionStore = sessions.NewCookieStore(
+		[]byte(os.Getenv("SESSION_AUTHENTICATION_KEY")),
+		ek,
+	)
 
 	// データベースへ接続
 	// ドライバにはSQLiteを使って、
@@ -37,7 +78,10 @@ func main() {
 	http.HandleFunc("/save", hs.SaveHandler)
 	http.HandleFunc("/summary", hs.SummaryHandler)
 
-	fmt.Println("http://localhost:8080 で起動中...")
+	// fmt.Println("http://localhost:8080 で起動中...")
 	// HTTPサーバを起動する
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+
+	log.Println(http.ListenAndServe(":"+port, nil))
+
 }
